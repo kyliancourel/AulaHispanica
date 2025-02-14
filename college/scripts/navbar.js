e9bcea47ecfdd8b32e3da2fc6ff6f8047227e119
Kylian Courel <kyliancourel@Kylians-MacBook-Pro.local>
2025-02-15 00:18:51 +0100

    création page accueil > collège

---
+++ college/scripts/navbar.js
@@ -0,0 +1,17 @@
+// Chargement de la navbar
+fetch('https://kyliancourel.github.io/AulaHispanica/college/pages/navbar.html') // Ajout du préfixe '/AulaHispanica'
+  .then(response => {
+    if (!response.ok) throw new Error(`Erreur ${response.status}: ${response.statusText}`);
+    return response.text();
+  })
+  .then(data => {
+    // Insérer le contenu de la navbar dans le conteneur
+    document.getElementById('navbar-container').innerHTML = data;
+
+    // Ajouter dynamiquement la feuille de style navbar.css
+    const linkElement = document.createElement('link');
+    linkElement.rel = 'stylesheet';
+    linkElement.href = 'https://kyliancourel.github.io/AulaHispanica/college/styles/navbar.css'; // Chemin du fichier CSS
+    document.head.appendChild(linkElement);
+  })
+  .catch(error => console.error('Erreur lors du chargement de la navbar :', error));
